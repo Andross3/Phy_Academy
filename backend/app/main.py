2cb76e5bdd4fbe78d773a0e4d7db69de546624ae
Andres Chavez <t4t4x777@gmail.com>
2025-05-15 21:33:33 -0400

    Actualizacion del main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,23 +1,14 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from backend.app.core.database import iniciar_base_datos
+from backend.app.core.extensions import db
+from backend.app.routes.auth_routes import auth_bp
 
 app = Flask(__name__)
 CORS(app)  # Para permitir llamadas desde el frontend
-
-@app.route("/api/login", methods=["POST"])
-def login():
-    data = request.get_json()
-    email = data.get("email")
-
-    if not email:
-        return jsonify({"error": "Email es requerido"}), 400
-
-    if email.endswith("@est.com"):
-        return jsonify({"role": "estudiante"}), 200
-    elif email.endswith("@doc.com"):
-        return jsonify({"role": "docente"}), 200
-    else:
-        return jsonify({"error": "Dominio no permitido"}), 403
+iniciar_base_datos(app)
+#registrar los Blueprints de las rutas
+app.register_blueprint(auth_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
