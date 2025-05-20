576def3e2d745dca06a2f4edb527e91dd588575d
elena <bernabemary0101@gmail.com>
2025-05-19 23:33:36 -0400

    cambio de color del shell

--- frontend/src/components/resultadoCompilacion.jsx
+++ frontend/src/components/resultadoCompilacion.jsx
@@ -1,11 +1,15 @@
+import { FaCode } from "react-icons/fa";
 export default function ResultadoCompilacion({ resultado }) {
     return (
-      <div className="mt-4 border border-black bg-[#edeadd] w-full">
-        <div className="bg-[#ded9c6] border border-black border-b-0 px-3 py-1 w-fit -mt-3 ml-2 text-black font-semibold">
-          Shell
+      <div className="mt-10 border border-black bg-gray-300 w-full min-h-[150px]">
+        <div className="ml-12 flex items-center gap-2 bg-gray-200 border border-black border-b-0 px-3 py-1 w-fit -mt-3 ml-2 text-black font-semibold">
+          <FaCode /> Shell
         </div>
-        <div className="p-4 text-gray-600 whitespace-pre-wrap">
+        <div className="p-4 text-gray-800 whitespace-pre-wrap">
           {resultado ? resultado : 'Respuesta del programa'}
+           <hr className=" border-t border-dashed border-gray-500 my-2" />
+          <hr className=" mt-5 border-t border-dashed border-gray-500 my-2" />
+           <hr className=" mt-5 border-t border-dashed border-gray-500 my-2" />
         </div>
       </div>
     );
