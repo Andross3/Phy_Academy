e534cbda11a02df7d88e7c67500eedaa81b4694f
Andres Chavez <t4t4x777@gmail.com>
2025-05-19 22:08:45 -0400

    actualizacion del archivo automatizar

--- backend/sandbox/automatizar.py
+++ backend/sandbox/automatizar.py
@@ -1,30 +1,33 @@
 import os, re
 import subprocess
 
-def formatoErrores(texto_error_capturado):
-    lineas_error = re.findall(r'File ".*", line (\d+)', texto_error_capturado)
-    tipo_error = re.search(r'(\w+Error|Exception): (.+)', texto_error_capturado)
-    descripcion_error = tipo_error.group(2) if tipo_error else texto_error_capturado.strip()
+class Compilar_Codigo:
 
-    print(f'Linea: {lineas_error[-1]}')
-    print(f'Tipo: {tipo_error.group(1)}')
-    print(f'Descripcion del error: {descripcion_error}')
+    def __init__(self, string_codigo):
+        self.codigo = string_codigo
+        self.archivo_py = 'pepe.py'
 
-ruta_actual = os.getcwd()
-print(ruta_actual)
+    def convertir(self):
+        carpeta_destino = "sandbox/archivos_compilacion"
+        # os.makedirs(carpeta_destino, exist_ok=True)
+        ruta_archivo = os.path.join(carpeta_destino, self.archivo_py)
+        with open(ruta_archivo, "w", encoding="utf-8") as archivo:
+            archivo.write(self.codigo)
 
-resultados = []
-
-for codigo in os.listdir():
-    if codigo.endswith('.py') and codigo != 'automatizar.py':
-        # listaCodigos.append(codigo)
-        resultado = subprocess.run(["docker", "run", "--rm", "-v", os.getcwd() + ":/app", "sandbox", "python", f'{codigo}'], 
+    def compilar(self):
+        resultado = subprocess.run(["docker", "run", "--rm", "-v", f"{os.getcwd()}/sandbox/archivos_compilacion:/app", "sandbox", "python", f'{self.archivo_py}'], 
                         capture_output=True, text=True)
         if resultado.stderr:
-            formatoErrores(resultado.stderr)
-            
+            self.formatoErrores(resultado.stderr)
+            # print('error')
         else:
-            resultados.append(resultado.stdout)
+            print('todo bien')
         
+    def formatoErrores(self, texto_error_capturado):
+        lineas_error = re.findall(r'File ".*", line (\d+)', texto_error_capturado)
+        tipo_error = re.search(r'(\w+Error|Exception): (.+)', texto_error_capturado)
+        descripcion_error = tipo_error.group(2) if tipo_error else texto_error_capturado.strip()
 
-# print(resultados)
+        print(f'Linea: {lineas_error[-1]}')
+        print(f'Tipo: {tipo_error.group(1)}')
+        print(f'Descripcion del error: {descripcion_error}')
