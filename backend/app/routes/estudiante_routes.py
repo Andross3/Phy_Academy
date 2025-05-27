ab5075090304def3f2851b436179281de5296bf6
Pierre-Rodri <solizcespedesrodrigo@gmail.com>
2025-05-27 13:28:33 -0400

    corregi el archivo seleccionarRol

--- backend/app/routes/estudiante_routes.py
+++ backend/app/routes/estudiante_routes.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
 from app.controllers.registro.estudiante_controller import registrar_estudiante
 
-estudiante_bp = Blueprint("estudiante", __name__, url_prefix="/estudiantes")
-estudiante_bp.route("/registrar", methods=["POST"])(registrar_estudiante)
+estudiante_bp = Blueprint("estudiante_bp", __name__, url_prefix="/estudiantes")
+
+@estudiante_bp.route("/registrar", methods=["POST"])
+def registrar():
+    return registrar_estudiante()
 
