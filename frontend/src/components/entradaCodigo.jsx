7eb96e5385e2a27be28e2de8be1abc61dcdf811b
Anahi44 <gamboaanahi46@gmail.com>
2025-05-26 22:59:34 -0400

    Pull con la rama de entrada

--- frontend/src/components/entradaCodigo.jsx
+++ frontend/src/components/entradaCodigo.jsx
@@ -4,58 +4,7 @@ import Editor from "react-simple-code-editor";
 import Prism from "prismjs";
 import { FaPython } from "react-icons/fa";
 import "prismjs/components/prism-python";
-import 'prismjs/themes/prism-okaidia.css'; // Puedes cambiar el tema
-
-/*const EntradaCodigo = ({ onChangeCode }) => {
-
-    const palabrasResaltadas = ["print", "def", "return", "import"];
-
-    const [code, setCode] = useState("");
-
-    const handleChange = (event) => {
-        // setCode(event.target.value);
-        const nuevoCodigo = event.target.value;
-        setCode(nuevoCodigo);
-        onChangeCode(nuevoCodigo);
-    };
-
-    const resaltarCodigo = (codigo) => {
-        return codigo.split(/\s+/).map((palabra, index) => (
-            palabrasResaltadas.includes(palabra) ?
-                <span key={index} style={{ color: "cyan" }}>{palabra} </span>
-                : <span key={index}>{palabra} </span>
-        ));
-    };
-
-    return (
-        <>
-            <div className="flex items-center bg-black border border-white border-b-0 p-2 w-fit -mt-2 ml-12 text-white font-semibold space-x-2">
-                <FaPython size={20} className="text-blue-400" />
-                <p className="text-white">Tarea_1.py</p>
-            </div>
-            <div className="border border-white bg-black w-full h-[350px] overflow-hidden">
-                <div className="flex p-2">
-                    <div className="text-right pr-4 pt-8 text-gray-400 w-10 flex flex-col items-end">
-                        {code.split("\n").map((_, index) => (
-                            <div className="text-blue-400" key={index}>{index + 1}
-                            </div>))}
-                    </div>
-                    <textarea
-                        className="flex-1 bg-black text-white border border-gray-600 p-4 pt-8 resize-none h-full overflow-auto"
-                        style={{
-                            scrollbarWidth: "thin",
-                            scrollbarColor: "#555 #222"
-                        }}
-                        value={code}
-                        onChange={handleChange}
-                        rows={15} />
-                </div>
-            </div>
-        </>
-    );
-};
-
-export default EntradaCodigo;*/
+import 'prismjs/themes/prism-okaidia.css'; 
 
 const EntradaCodigo = ({ onChangeCode }) => {
   const [code, setCode] = useState("");
