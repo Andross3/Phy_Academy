6c758fbe6260e610c4bfb54d113699a35c18754f
Andres Chavez <t4t4x777@gmail.com>
2025-06-12 23:56:39 -0400

    fix(ejecutar_codigo): corrige la consulta post al sandbox

--- backend/sandbox/Container.py
+++ backend/sandbox/Container.py
@@ -1,37 +1,43 @@
-import subprocess
-import docker
+import requests
 
 class Contenedor:
     def __init__(self, id, instancia):
-        self.cliente_docker = docker.from_env()
         self.id = id
         self.instancia = instancia
         self.ip = ''
-        self.limpiado = False
+        self.limpio = False
+# ejecuta el codigo en un contenedor
+    def ejecutar_codigo(self, codigo):
+        url_sandbox = f'http://{self.ip}:8080/' # ver esto mas 
+        payload = {
+            "code": codigo,
+            "timeoutMs": 1000,
+        }
+        
+        try:
+            respuesta = requests.post(url=url_sandbox, json=payload, timeout=2)
+            respuesta.raise_for_status()
+            data = respuesta.json()
+            return data
+            # print(data)
+        except requests.exceptions.RequestException as e:
+            # print(e)
+            return f'Error al conectar al sandbox {e}'
 
-    def ejecutar_codigo(self, job, callback):
-
-        # resultado = subprocess.run(['docker', 'run', '--rm'])
-        # resultado = subprocess.run(["docker", "run", "--rm", "-v", f"{os.getcwd()}/backend/sandbox/archivos_compilacion:/app", "sandbox", "python", f'{self.archivo_py}'], 
-        #                 capture_output=True, text=True)
-        # if resultado.stderr:
-        #     self.formatoErrores(resultado.stderr)
-        # else:
-        #     print(resultado.stdout.strip())
-        print(f'hola llego tu { job }')
-
-    def getInstancia(self):
+# Obtener instancia (sandbox)
+    def get_instancia(self):
         return self.instancia
-    
-    def setIp(self, newIP):
+# insertar ip
+    def set_ip(self, newIP):
         self.ip = newIP
-
-    def cleanUp(self, callback):
-
-        ### parar
-        # remover
-        # marcar como limpio
-        if self.limpiado == True:
+# limpiar contenedor
+    def clean_up(self):
+        if self.limpio == True:
             print('esta limpio')
         else:
-            print('esta en ejecusion')
+            try:
+                self.instancia.stop()
+                self.instancia.remove(force=True)
+                self.limpio = True
+            except Exception as e:
+                print(f"Error en limpiar sandbox {e}")
