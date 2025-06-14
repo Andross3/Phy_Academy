9f594e0897a86826e8bc1342c1df90b28e04271c
Andres Chavez <t4t4x777@gmail.com>
2025-06-14 15:00:42 -0400

    feat(ejecutador_controller): anade controlador para la ejecucion del sanbox desde el backend

--- backend/app/controllers/ejecutador_controller.py
+++ backend/app/controllers/ejecutador_controller.py
@@ -1,6 +1,13 @@
 from flask import request, jsonify
-
+from backend.sandbox.sandbox_interface import Sandbox
 
 def ejecutar_codigo(request):
     datos = request.get_json()
-    return jsonify({'mensaje': 'codigo recibido'}), 200
+    codigo = datos.get('code')
+
+    sandbox_prueba = Sandbox(1, codigo)
+    sandbox_prueba.correr_codigo()
+    sandbox_prueba.crear_pool()
+    output_codigo = sandbox_prueba.respuesta()
+    
+    return jsonify({'mensaje': output_codigo}), 200
