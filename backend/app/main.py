f62420af161614f2f04ee227db7d252712675e86
Andres Chavez <t4t4x777@gmail.com>
2025-06-18 11:42:45 -0400

    fix: arregla la migracion a la base de datos

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,6 @@ from flask_cors import CORS
 from backend.app.core.database import iniciar_base_datos
 from backend.app.core.extensions import db
 from backend.app.routes.auth_routes import auth_bp
-from sqlalchemy import text
 from backend.app.routes.ejecutador_routes import ejecutador_bp
 from backend.app.routes.tareas_routes import tarea_bp  # Importa tarea_bp
 from flask_migrate import Migrate
@@ -11,6 +10,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 CORS(app)  # Para permitir llamadas desde el frontend
 iniciar_base_datos(app)
+migrate = Migrate(app, db)
 #registrar los Blueprints de las rutas
 app.register_blueprint(auth_bp)
 app.register_blueprint(ejecutador_bp)
