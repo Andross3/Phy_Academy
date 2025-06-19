5f9bec67c48dea4a4dedd7e98493cc270cdc1bd3
vcc1406 <valeriacabrera198833@gmail.com>
2025-06-18 22:58:05 -0400

    Fix cambios vista docente

--- frontend/src/pages/paginaTareaDocente.jsx
+++ frontend/src/pages/paginaTareaDocente.jsx
@@ -257,8 +257,7 @@ export default function PaginaTareaDocente() {
         </button>
         <button
           className="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-900 disabled:opacity-50"
-          disabled={!tipoTarea}
-          // onClick={imprimirValores}
+          disabled={!tipoTarea || isSubmitting}
           onClick={enviarDatos}
         >
           {isSubmitting ? 'Enviando...' : 'Publicar'}
