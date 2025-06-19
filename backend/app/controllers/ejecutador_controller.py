b3676131c6c8c562540a5c4bc2b3b25ebfb78ea1
Andres Chavez <t4t4x777@gmail.com>
2025-06-19 19:49:38 -0400

    feat(traduccion): anade las traducciones al espanol en el compilador de codigo

--- backend/app/controllers/ejecutador_controller.py
+++ backend/app/controllers/ejecutador_controller.py
@@ -1,6 +1,9 @@
 import re
 from flask import request, jsonify
 from backend.sandbox.sandbox_interface import Sandbox
+from googletrans import Translator
+
+traductor = Translator()
 
 PALABRAS_PROHIBIDAS = [
     'for'  
@@ -30,10 +33,10 @@ def ejecutar_codigo(request):
     mensaje = output_codigo.get('stdout','')
     mensajeError = output_codigo.get('stderr','')
     if mensaje != "":
-     return jsonify({'mensaje': mensaje}), 200
+        return jsonify({'mensaje': mensaje}), 200
     else:
-     mensajeBonito = formatoErrores(mensajeError)
-     return jsonify({'mensaje': mensajeBonito}), 200
+        mensajeBonito = formatoErrores(mensajeError)
+        return jsonify({'mensaje': mensajeBonito}), 200
 
 def formatoErrores(texto_error_capturado):
     lineas_error = re.findall(r'File ".*", line (\d+)', texto_error_capturado)
@@ -42,13 +45,30 @@ def formatoErrores(texto_error_capturado):
     linea = lineas_error[-1] if lineas_error else "desconocida"
     tipo = tipo_error.group(1) if tipo_error else "Error desconocido"
     descripcion = tipo_error.group(2) if tipo_error else texto_error_capturado.strip()
+    #traduccion del ingles a español
+    tipo_es = traductor.translate(text=separar_palabras(tipo), src='en', dest='es')
+    descripcion_es = traductor.translate(text=descripcion, src='en', dest='es')
 
     mensajeBonito = (
         f"Error:\n"
-        f"Tipo: {tipo}\n"
+        f"Tipo: {tipo_es.text}\n"
         f"Línea: {linea}\n"
-        f"Descripción: {descripcion}"
+        f"Descripción: {descripcion_es.text}"
     )
 
     return mensajeBonito
-     
+
+def separar_palabras(cadena):
+    pos = []
+    cont = 0
+    for i in cadena:
+        if i.isupper():
+            pos.append(cont)
+        cont=cont+1
+
+    palabras = []
+    for i in range(len(pos)):
+        inicio = pos[i]
+        fin = pos[i+1] if i+1 < len(pos) else len(cadena)
+        palabras.append(cadena[inicio:fin])
+    return ' '.join(palabras)
